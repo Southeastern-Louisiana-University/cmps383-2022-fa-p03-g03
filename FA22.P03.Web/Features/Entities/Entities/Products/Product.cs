b1965c5da4107781151138746e561bbfa0ff7296
Jdillon297 <JACOBDILLON119@GMAIL.COM>
2022-09-04 23:54:59 -0500

    feat/Products
    added Products entity

---
+++ FA22.P03.Web/Features/Entities/Entities/Products/Product.cs
@@ -0,0 +1,13 @@
+﻿using FA22.P03.Web.Features.Items;
+
+namespace FA22.P03.Web.Features.Entities.Entities.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<ItemDtos> Items { get; set; }
+    }
+
+}
--- FA22.P03.Web/Features/Entities/Entities/Products/ProductDto.cs
+++ FA22.P03.Web/Features/Entities/Entities/Products/ProductDto.cs
@@ -2,13 +2,6 @@
 
 namespace FA22.P03.Web.Features.Products;
 
-public class Product
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public ICollection<ItemDtos> Items { get; set; }
-}
 
 
 public class ProductDto
